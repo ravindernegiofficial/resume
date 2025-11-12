0c24c595c89f2561cc7aa363c5e274f86a9c596c
Ravinder Negi <99467116+ravindernegiofficial@users.noreply.github.com>
2025-11-12 15:45:29 +0530

    assets
    img assets

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -170,11 +170,11 @@ var projectData = {
 👉 <strong>Visit:</strong> <a href="https://www.bombaycafepalma.com/" target="_blank">bombaycafepalma.com</a>
 `,
     images: [
-      "assets/img/ravindernegi-portfolio-01.jpg",
-      "assets/img/resturant-website.jpg",
-      "assets/img/resturant-website-2.jpg",
-      "assests/img/resturant-website-3.jpg",
-        "assests/img/resturant-website-4.jpg"
+      "./assets/img/ravindernegi-portfolio-01.jpg",
+      "./assets/img/resturant-website.jpg",
+      "./assets/img/resturant-website-2.jpg",
+      "./assests/img/resturant-website-3.jpg",
+        "./assests/img/resturant-website-4.jpg"
     ]
   },
   meta1: {
